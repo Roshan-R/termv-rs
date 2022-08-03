5aa6115d8b6c94d6e1579e4185b0d5dd5260a76c
Roshan-R <roshanr2001@gmail.com>
2022-08-03 22:06:26 +0530

    feat: update triggers only if json file is older than one day

--- src/download.rs
+++ src/download.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::path::Path;
+use std::time::{Duration, SystemTime};
 
 use whoami::username;
 
@@ -49,7 +50,25 @@ impl Downloader {
         println!("Done!");
     }
 
+    fn should_update(&self) -> bool {
+        let metadata = fs::metadata(self.etag_path.as_str()).unwrap();
+
+        let last_modified = metadata.modified().unwrap();
+        let now = SystemTime::now();
+
+        let one_day = Duration::new(86400, 0);
+
+        let difference = now
+            .duration_since(last_modified)
+            .expect("Error while calculating difference in time");
+
+        difference > one_day
+    }
+
     pub fn update_if_changed(&self) {
+        if !self.should_update() {
+            return;
+        };
         println!("Checking for updates..");
         let resp = ureq::get("https://iptv-org.github.io/iptv/channels.json")
             .set("Accept-Encoding", "gzip")
