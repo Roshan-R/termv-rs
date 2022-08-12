c3ad2c313acb5e33e6a79b651d27c65029286d92
Roshan-R <roshanr2001@gmail.com>
2022-08-12 15:48:26 +0530

    linux: fixed crossplatform support

--- src/selector.rs
+++ src/selector.rs
@@ -1,19 +1,16 @@
-use std::process::Command;
-use std::process::Stdio;
-use std::io::Write;
-//extern crate skim;
-//use skim::SkimOptionsBuilder;
-
-//use skim::prelude::*;
-
 #[cfg(target_os = "windows")]
 pub fn get_user_selection(buffer: String) -> String {
-
+    use std::io::Write;
+    use std::process::Command;
+    use std::process::Stdio;
     let selectors = buffer;
 
-
     let mut fzf = Command::new("fzf")
-        .args(["--reverse", "--header", "Select channel (press Escape to exit)"])
+        .args([
+            "--reverse",
+            "--header",
+            "Select channel (press Escape to exit)",
+        ])
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()
@@ -24,33 +21,33 @@ pub fn get_user_selection(buffer: String) -> String {
         .write_all(selectors.to_string().as_bytes())
         .expect("Failed to write to stdin");
 
-    let output = fzf.wait_with_output().expect("Failed to read stdout of fzf");
+    let output = fzf
+        .wait_with_output()
+        .expect("Failed to read stdout of fzf");
     String::from_utf8_lossy(&output.stdout).to_string()
-    
 }
 
-
 #[cfg(not(target_os = "windows"))]
-pub fn setup_skim() -> SkimOptionsBuilder {
+pub fn get_user_selection(buffer: String) -> String {
+    extern crate skim;
+    use skim::prelude::SkimOptionsBuilder;
+    use skim::prelude::*;
+
+    use std::io::Cursor;
     let options = SkimOptionsBuilder::default()
         .height(Some("100%"))
         .layout("reverse")
         .header(Some("Select channel (press Escape to exit)"))
         .build()
         .unwrap();
-}
-
-#[cfg(not(target_os = "windows"))]
-pub fn get_user_selection(buffer: String) -> String{
-    let options = setup_skim();
 
     let item_reader = SkimItemReader::default();
-    let items = item_reader.of_bufread(Cursor::new(new_input));
+    let items = item_reader.of_bufread(Cursor::new(buffer));
 
     let skim_output = Skim::run_with(&options, Some(items)).unwrap();
 
     if skim_output.is_abort {
-        return;
+        return "null".to_string();
     }
 
     let s = skim_output
@@ -59,6 +56,5 @@ pub fn get_user_selection(buffer: String) -> String{
         .unwrap()
         .output()
         .to_string();
+    return s;
 }
-
-
